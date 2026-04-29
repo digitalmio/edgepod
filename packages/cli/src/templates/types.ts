ff3494c2c4f66753921a90da7df5e425e43c11a1
Maciej Ziehlke <mac@dmio.co>
2026-04-29 23:23:54 +0100

    feat: improve type definitions with additional comments for Env, Variables, and User types

--- packages/cli/src/templates/types.ts
+++ packages/cli/src/templates/types.ts
@@ -1,14 +1,19 @@
 export const genTypesTemplate = () => `import * as schema from "./schema";
 import type { EdgePodContext } from "@edgepod/server";
 
+// Any env vars added to .env will be automatically deployed via Wrangler
+// Please add any custom env vars you need to the Env type below for type safety in your functions
 type Env = {
   SECRET_KEY: string;
 }
 
+// Those are request lifecycle variables that you might want to use in your functions
 type Variables = {
   traceId: string;
 }
 
+// The User type represents the shape of the user object available in your functions via
+// the 'context.user' property from decoded JWT
 type User = {
   id: string;
   isAdmin: boolean;
